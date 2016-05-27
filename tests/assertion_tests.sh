db1c3dc7d79875217bb21b3e14005e0e6ea0e32a
Rob Westgeest <rob@qwan.eu>
2016-05-27 13:18:19 +0100

    using asser_equals in its test

--- tests/assertion_tests.sh
+++ tests/assertion_tests.sh
@@ -1,16 +1,16 @@
 test_assert_equals_shows_message_on_error() {
-  [[ "Assertion failed; expected '0', but was: '1'" == $(assert_equals 0 1)  ]]
-  [[ "Assertion failed; expected '1', but was: '2'" == $(assert_equals 1 2)  ]]
+  assert_equals "Assertion failed; expected '0', but was: '1'" "$(assert_equals 0 1)"
+  assert_equals "Assertion failed; expected '1', but was: '2'" "$(assert_equals 1 2)"
 }
 test_assert_equals_returns_1_on_error() {
-  [[ 1 == $(a_silent_run_of assert_equals 0 1)$? ]]
+  assert_equals 1 "$(a_silent_run_of assert_equals 0 1)$?"
 }
 test_assert_equals_exits_on_error() {
-  [[ 1 == $(a_silent_run_of assert_equals 0 1; return 0)$? ]]
+  assert_equals 1 "$(a_silent_run_of assert_equals 0 1; return 0)$?"
 }
 test_assert_equals_shows_nothing_on_success() {
-  [[ "" == $(assert_equals 1 1)  ]]
+  assert_equals "" "$(assert_equals 1 1)"
 }
 test_assert_equals_returns_0_on_success() {
-  [[ 0 == $(a_silent_run_of assert_equals 0 0)$? ]]
+  assert_equals 0 "$(a_silent_run_of assert_equals 0 0)$?"
 }
